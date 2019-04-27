995ea10e8ec3ac7fa3546ead379659c4b492e6b3
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-27 10:59:30 -0500

    Got basic setup going

---
+++ challenge_2/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }, 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'css-loader',
+        query: {
+          modules: true,
+        localIdentName: '[name]__[local]___[hash:base64:5]',
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./client/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
