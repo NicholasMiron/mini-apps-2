200e20a12b7588b38ac849290c3ec4e7371fe142
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-29 14:02:24 -0500

    building and linting correctly for now

--- challenge_3/.eslintrc.js
+++ challenge_3/.eslintrc.js
@@ -1,3 +1,32 @@
 module.exports = {
   extends: './node_modules/eslint-config-airbnb-base/index.js',
+  "globals": {
+    "document": true,
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+      "modules": true,
+      "experimentalObjectRestSpread": true
+    }
+  },
+  plugins: ['react'],
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "rules": {
+    "comma-dangle": 0,
+    "react/jsx-uses-vars": 1,
+    "react/display-name": 1,
+    "no-unused-vars": "warn",
+    "no-console": 1,
+    "no-unexpected-multiline": "warn"
+  },
+  "settings": {
+    "react": {
+      "pragma": "React",
+      "version": "15.6.1"
+    },
+  },
 };
