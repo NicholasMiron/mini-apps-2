ccfe8f26ac03a8cd9b9e3a11325233ca2ec25fd9
Nicholas Miron <nicholasmiron@gmail.com>
2019-04-29 13:36:25 -0500

    Fixed type in entry for webpack

--- challenge_3/webpack.config.js
+++ challenge_3/webpack.config.js
@@ -1,45 +1,45 @@
 const path = require('path');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: production,
-  entry: 'client/index.js',
+  mode: 'production',
+  entry: './client/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: 'style-loader',
           },
           {
             loader: 'css-loader',
             query: {
               modules: true,
               localIdentName: '[name]__[local]___[hash:base64:5]',
-            }
-          }
-        ]
+            },
+          },
+        ],
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
-      }
-    ]
+        loader: 'html-loader',
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./client/index.html",
-      filename: "./index.html"
-    })
+      template: './client/index.html',
+      filename: './index.html',
+    }),
   ],
-}
+};
